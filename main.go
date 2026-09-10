91421d06a2f94887d6df856df7680e53724c4d54
Areeba Sajjad <areebasatti949@gmail.com>
2026-09-10 14:13:42 +0500

    Add HTML form and CORS support for user creation API

--- main.go
+++ main.go
@@ -51,6 +51,15 @@ func main() {
 }
 
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
 		return
